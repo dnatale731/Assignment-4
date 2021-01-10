f780863856260252651003c2a2ee41ab3655f3ea
Domenico Natale <dnatale731@gmail.com>
2021-01-10 00:19:21 -0500

    Part 3 & 4 Complete

--- OneDrive/Documents/Coding Folder/TTP Assignments/Assignment-4/script.js
+++ OneDrive/Documents/Coding Folder/TTP Assignments/Assignment-4/script.js
@@ -1,20 +1,50 @@
 let button1Pressed = false
 let button2Pressed = false
 let hovered = false
+let masterPassword = "12345678"
+
 button1 = document.querySelector("#button1")
 button2 = document.querySelector("#button2")
 text = document.querySelector("#text")
-dontHover = document.querySelector(".dontHover h1")
+dontHover = document.querySelector(".dontHover h2")
+submit = document.querySelector("#submit")
+formHeader = document.querySelector("#formHeader")
 
 //Buttons change the text box
-button1.addEventListener("click", function(){ text.innerText="I'm right"; button1Pressed = true })
-button2.addEventListener("click", function(){ text.innerText="No, I'm right!"; button2Pressed = true })
-dontHover.addEventListener("mouseover", function(){ alert("Hey, I told you not to hover over me!"); hovered = true })
+button1.addEventListener("click", function() { 
+    text.innerText="I'm right"
+    button1Pressed = true 
+})
+button2.addEventListener("click", function() { 
+    text.innerText="No, I'm right!"
+    button2Pressed = true
+})
+
+//Creates an alert when you hover over the h2 element
+dontHover.addEventListener("mouseover", function() { 
+    alert("Hey, I told you not to hover over me!")
+    hovered = true
+})
+
+//Submits the values of the form
+submit.addEventListener("click", function() {
+    password = document.querySelector("#password").value
+    
+    //Checks if the password is correct
+    if(password === masterPassword) {
+        formHeader.innerText = "Password Correct. Thank You For Filling Out My Form"
+    }
+    else {
+        alert("Incorrect Password")
+    }
+})
+
+
 
 if(button1Pressed && button2Pressed) {
     //Original plan didn't work, will try something else when doing CSS
 }
 
 if(hovered) {
-    //Will Implement Part 3 Here
+    //Original plan didn't work, will try something else when doing CSS
 }
